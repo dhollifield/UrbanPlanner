18edd3519667edf9d7d4ff676695c7785637b958
dhollifield <deannahollifield@gmail.com>
2023-02-04 13:01:58 -0600

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,51 @@
+﻿namespace UrbanPlanner;
+
+public class Building
+{
+    private string _designer = "Deanna";
+    private DateTime _dateConstructed;
+    private string _address;
+    private string _owner;
+
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+
+    public string Designer 
+    { 
+        get { return _designer; } 
+    }
+
+    public DateTime DateConstructed 
+    { 
+        get { return _dateConstructed; }
+    }
+    public string Address 
+    {
+        get { return _address; }
+    }
+
+    public string Owner
+    {
+        get { return _owner; }
+    }
+    public double Volume 
+    {
+        get { return Width * Depth * (3 * Stories); }
+    }
+
+    public Building(string address)
+    {
+        _address = address;
+    }
+
+    public void Construct()
+    {
+        _dateConstructed = DateTime.Now;
+    }
+
+    public void Purchase(string owner)
+    {
+        _owner = owner;
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace UrbanPlanner;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Building FiveOneTwoEigth = new Building("512 8th Avenue");
+        FiveOneTwoEigth.Stories = 15;
+        FiveOneTwoEigth.Width = 98.27;
+        FiveOneTwoEigth.Depth = 47.68;
+        FiveOneTwoEigth.Purchase("Michael");
+        FiveOneTwoEigth.Construct();
+
+        Building TwoOneSeventh = new Building("21 7th Street");
+        TwoOneSeventh.Stories = 7;
+        TwoOneSeventh.Width = 42.50;
+        TwoOneSeventh.Depth = 37.15;
+        TwoOneSeventh.Purchase("Audrey");
+        TwoOneSeventh.Construct();
+
+        List<Building> buildings = new List<Building>();
+        buildings.Add(FiveOneTwoEigth);
+        buildings.Add(TwoOneSeventh);
+
+        foreach (Building building in buildings)
+        {
+            Console.WriteLine($"{building.Address}------------------");
+            Console.WriteLine($"Designed by: {building.Designer}");
+            Console.WriteLine($"Constructed on: {building.DateConstructed}");
+            Console.WriteLine($"Purchased by: {building.Owner}");
+            Console.WriteLine($"{building.Volume} cubic meters of space");
+            Console.WriteLine("______________________________");
+        }
+
+    }
+}
